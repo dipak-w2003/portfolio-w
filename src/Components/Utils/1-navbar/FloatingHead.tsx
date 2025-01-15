d95c1312b41933be1493b6e01cfe7f853942b761
dipak-w2003 <dipak2003w@gmail.com>
2025-01-15 20:37:41 +0545

    added motion on opening minibar : do homepage welcome

--- src/Components/Utils/1-navbar/FloatingHead.tsx
+++ src/Components/Utils/1-navbar/FloatingHead.tsx
@@ -3,10 +3,14 @@ import { ITOGGLE } from "./toggle";
 
 const FloatingHead: React.FC<ITOGGLE> = ({ ToggleTorF }) => {
   return (
-    <div className="w-[100px] overflow-hidden h-[100px] bg-gradient-to-b from-[#64a3b4] to-[#d3d3d3] rounded-full flex justify-center items-center">
+    <div
+      className="w-[80px] overflow-hidden h-[80px] bg-gradient-to-b from-[#64a3b4] to-[#d3d3d3] rounded-full flex justify-center items-center
+    
+    "
+    >
       <div
         onClick={() => ToggleTorF()}
-        className="bg-[url('https://static.vecteezy.com/system/resources/previews/028/627/081/non_2x/watercolor-anime-character-high-quality-illustration-vector-background-photo.jpg')] bg-cover bg-no-repeat w-[96px] h-[96px] rounded-full animate-pulse"
+        className="bg-[url('https://static.vecteezy.com/system/resources/previews/028/627/081/non_2x/watercolor-anime-character-high-quality-illustration-vector-background-photo.jpg')] bg-cover bg-no-repeat w-[75px] h-[75px] rounded-full animate-pulse "
       ></div>
     </div>
   );
--- src/Components/Utils/1-navbar/MiniBar/MiniBar.tsx
+++ src/Components/Utils/1-navbar/MiniBar/MiniBar.tsx
@@ -6,7 +6,12 @@ import BottomContent from "./BottomContent";
 
 const MiniBar: React.FC<ITOGGLE> = ({ ToggleTorF }) => {
   return (
-    <nav className=" w-[200px] h-[450px] rounded-[15px] bg-gradient-to-b from-[#64a3b4] to-[#d3d3d3] relative  overflow-hidden p-2">
+    <nav
+      className=" w-[200px] h-[450px] rounded-[15px] bg-gradient-to-b from-[#64a3b4] to-[#d3d3d3] relative  overflow-hidden p-2
+      motion-preset-pop motion-duration-500
+      "
+      onMouseLeave={ToggleTorF}
+    >
       <CurvedSvg />
       {/* ? contents */}
       <div className=" w-full h-full *:h-[50%] *:w-full  flex flex-col gap-2 justify-center *:relative *:z-20 *:overflow-hidden *:rounded-md">
