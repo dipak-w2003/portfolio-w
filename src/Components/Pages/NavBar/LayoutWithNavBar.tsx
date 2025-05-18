0870c4c352719169c9a6e250a735d4ab73e135d3
dipak-w2003 <dipaktamang2003w@gmail.com>
2025-05-18 12:56:06 +0545

    production:error-fixed-- commented-unused-functions

--- src/Components/Pages/NavBar/LayoutWithNavBar.tsx
+++ src/Components/Pages/NavBar/LayoutWithNavBar.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import NavBar from "./NavBar";
 interface IProps {
   children?: ReactNode;
@@ -18,36 +18,38 @@ const LayOutWithNavBar = (props: IProps) => {
     Todo: Confusion about less then or more than in order to find current screen is i.e: sm, xs, lg else through out `px`
     
     */
-function MediaQueryWidget() {
-  const [w, setW] = React.useState<number>(window.innerWidth);
-  React.useEffect(() => {
-    window.addEventListener("resize", () => {
-      setW(window.innerWidth);
-    });
-    return () => {
-      window.removeEventListener("resize", () => {
-        setW(window.innerWidth);
-      });
-    };
-  }, [w]);
 
-  function analyzeMediaQuery(width: number) {
-    let determine: string = "";
-    if (width >= 320) determine = "XXS";
-    if (width >= 480) determine = "XS";
-    if (width >= 640) determine = "SM";
-    if (width >= 768) determine = "MD";
-    if (width >= 1024) determine = "LG";
-    if (width >= 1280) determine = "XL";
-    if (width >= 1536) determine = "2XL";
-    return `${determine}`;
-  }
+/** @Commented */
+// function MediaQueryWidget() {
+//   const [w, setW] = React.useState<number>(window.innerWidth);
+//   React.useEffect(() => {
+//     window.addEventListener("resize", () => {
+//       setW(window.innerWidth);
+//     });
+//     return () => {
+//       window.removeEventListener("resize", () => {
+//         setW(window.innerWidth);
+//       });
+//     };
+//   }, [w]);
 
-  return (
-    <span className="widget-media-query fixed bottom-6 z-50 bg-cyan-900 text-2xl left-6 p-4 rounded-md text-white font-extrabold">
-      {analyzeMediaQuery(w)}:{w}px
-    </span>
-  );
-}
+//   function analyzeMediaQuery(width: number) {
+//     let determine: string = "";
+//     if (width >= 320) determine = "XXS";
+//     if (width >= 480) determine = "XS";
+//     if (width >= 640) determine = "SM";
+//     if (width >= 768) determine = "MD";
+//     if (width >= 1024) determine = "LG";
+//     if (width >= 1280) determine = "XL";
+//     if (width >= 1536) determine = "2XL";
+//     return `${determine}`;
+//   }
+
+//   return (
+//     <span className="widget-media-query fixed bottom-6 z-50 bg-cyan-900 text-2xl left-6 p-4 rounded-md text-white font-extrabold">
+//       {analyzeMediaQuery(w)}:{w}px
+//     </span>
+//   );
+// }
 
 export default LayOutWithNavBar;
